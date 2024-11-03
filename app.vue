b8afd3bffea8a8f8214d448463472637511cd950
Alita-oss <111301275+Alita-oss@users.noreply.github.com>
2024-11-03 11:32:06 +0100

    feat: basic layout, logo, favicon

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
     <v-app>
-        <h1>Hello World</h1>
+        <v-navigation-drawer>
+            <v-list>
+                <v-list-item title="Navigation drawer"></v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar title="Application bar">
+            <h1>SDH Lípa</h1>
+        </v-app-bar>
+
+        <v-main class="d-flex align-center justify-center" style="min-height: 300px"> Main Content </v-main>
     </v-app>
 </template>
