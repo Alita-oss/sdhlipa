38b8b58159621fe14c4cb8b3b3d2aef21dfc37c3
Alita-oss <111301275+Alita-oss@users.noreply.github.com>
2024-11-16 15:49:16 +0100

    feat: header, footer, install sass

--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { VAppBar, VAppBarTitle } from 'vuetify/components';
+</script>
+
 <template>
     <v-app>
         <v-navigation-drawer>
@@ -6,10 +10,39 @@
             </v-list>
         </v-navigation-drawer>
 
-        <v-app-bar title="Application bar">
-            <h1>SDH Lípa</h1>
-        </v-app-bar>
+        <VAppBar flat>
+            <!-- <VAppBarNavIcon> </VAppBarNavIcon> -->
+            <img class="logo" src="~/assets/images/logo.webp" alt="Logo hasičů" />
+            <VAppBarTitle tag="h1">SDH Lípa</VAppBarTitle>
+        </VAppBar>
 
-        <v-main class="d-flex align-center justify-center" style="min-height: 300px"> Main Content </v-main>
+        <v-main class="sdh-main">
+            <div>Main Content</div>
+            <v-footer class="sdh-footer">
+                <p>&copy; {{ new Date().getFullYear() }} SDH Lípa</p>
+            </v-footer>
+        </v-main>
     </v-app>
 </template>
+
+<style lang="scss" scoped>
+.logo {
+    margin-left: 1.25rem;
+    height: 3rem;
+    width: 3rem;
+}
+
+.sdh-main {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.sdh-footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-height: 2.5rem;
+    padding: 0.5rem;
+}
+</style>
