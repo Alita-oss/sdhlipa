50b0957fed29536b3178c19b56e6e1a0e11b65a5
Zdeňka Kameníková <111301275+Alita-oss@users.noreply.github.com>
2024-12-08 11:38:16 +0100

    feat: add pages, eslint (#4)

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+    { ignores: ['**/node_modules/**', '**/.nuxt/**', '**/.output/**'] },
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...pluginVue.configs['flat/essential'],
+    { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+    {
+        rules: {},
+    },
+    {
+        files: ['pages/**/*.vue'],
+        rules: {
+            'vue/multi-word-component-names': 'off',
+        },
+    },
+];
