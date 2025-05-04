b9d6dc36bdf1d039f478408ffcd2ea96ee37f7d5
Zdeňka Kameníková <111301275+Alita-oss@users.noreply.github.com>
2025-05-04 17:40:30 +0200

    feat(SDH-12): create history page (#9)
    * feat: history page
    
    * feat: history page

---
+++ components/historyView.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card class="history-card">
+        <v-card-title>Úspěchy na soutěžích</v-card-title>
+        <v-data-table :items="results" :headers="headers" class="results-table" dense />
+    </v-card>
+</template>
+
+<script setup>
+const headers = [
+    { text: 'Rok', value: 'year' },
+    { text: 'Soutěž', value: 'event' },
+    { text: 'Kategorie', value: 'category' },
+    { text: 'Umístění', value: 'placement' },
+];
+
+const results = ref([
+    { year: 2023, event: 'Okresní kolo', category: 'Muži', placement: '1. místo' },
+    { year: 2023, event: 'Okresní kolo', category: 'Děti', placement: '3. místo' },
+    { year: 2022, event: 'Okresní kolo', category: 'Juniorky', placement: '2. místo' },
+    { year: 2021, event: 'Krajské kolo', category: 'Muži', placement: '3. místo' },
+    { year: 2021, event: 'Krajské kolo', category: 'Junioři', placement: '3. místo' },
+]);
+</script>
+
+<style scoped>
+.history-card {
+    margin-top: 20px;
+    border-radius: 12px;
+}
+
+.results-table {
+    margin-top: 10px;
+}
+</style>
--- pages/history.vue
+++ pages/history.vue
@@ -1,7 +1,32 @@
 <template>
-    <h1>History</h1>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <h1 class="title">Historie týmu</h1>
+                <p class="intro">
+                    Náš sbor vznikl v roce 1875 a od té doby se aktivně účastníme soutěží v požárním sportu. Nachazíme
+                    se v malé vesničce Lípa. Kousek od Havlíčkova Brodu.
+                </p>
+            </v-col>
+            <v-col cols="12">
+                <HistoryView />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
-<script setup lang="ts"></script>
+<script setup>
+import HistoryView from '@/components/historyView.vue';
+</script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.title {
+    font-size: 2rem;
+    margin-bottom: 16px;
+}
+
+.intro {
+    font-size: 1.1rem;
+    margin-bottom: 24px;
+}
+</style>
